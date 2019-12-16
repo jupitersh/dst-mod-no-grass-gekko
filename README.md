83e0ca098e432201047e784124ea041f5ad2b306
jupitersh <arxhd.love@qq.com>
2019-12-16 16:09:07 +0800

    v1.0

---
+++ README.md
@@ -0,0 +1,13 @@
+# No Grass Gekko
+
+## Overview
+
+This is a mod for the game of Don't Starve Together which is available in the Steam Workshop. 
+
+The grass won't turn into grass gekko upon picking.
+
+## Changelog
+
+## License
+
+Released under the [GNU GENERAL PUBLIC LICENSE](https://www.gnu.org/licenses/gpl-3.0.en.html)
---
+++ modinfo.lua
@@ -0,0 +1,22 @@
+-- This information tells other players more about the mod
+name = "No Grass Gekko"
+description = "The grass won't turn into grass gekko upon picking"
+author = "Jupiter"
+version = "1.0"
+
+forumthread = ""
+
+-- This lets other players know if your mod is out of date, update it to match the current version in the game
+api_version = 10
+
+--This let's the game know that this mod doesn't need to be listed in the server's mod listing
+client_only_mod = false
+
+--Let the mod system know that this mod is functional with Don't Starve Together
+dst_compatible = true
+
+all_clients_require_mod = true 
+
+-- Can specify a custom icon for this mod!
+--icon_atlas = "123.xml"
+--icon = "123.tex"
